73c9ead2c64e998e98034489ba10656f5dc24ca7
Raj <146608157+RajDevX@users.noreply.github.com>
2025-05-08 16:29:33 +0530

    Add files via upload

---
+++ ascii.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter a value to find ascii ";
+    cin>>ch;
+    int ascii = ch;
+    cout<<ascii;
+    return 0;
+}
