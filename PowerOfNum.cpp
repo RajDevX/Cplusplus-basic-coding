4fb04c8c3c60edf59f4ee58fc81c7ad4bab4eb47
Raj <146608157+RajDevX@users.noreply.github.com>
2025-05-17 09:16:49 +0530

    Add files via upload

---
+++ PowerOfNum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int base,expo;
+    cin>>base>>expo;
+    int power = pow(base,expo);
+    cout<<power;
+    return 0;
+}
