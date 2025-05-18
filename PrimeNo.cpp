f89785222505ff3fcc5008dada525ca196993561
Raj <146608157+RajDevX@users.noreply.github.com>
2025-05-18 09:34:44 +0530

    Add files via upload

---
+++ PrimeNo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int num;
+  cin >> num;
+  int div = 0;
+  for (int i = 1; i <= num; i++)
+  {
+    if (num % i == 0)
+      div++;
+  }
+  if (div == 2)
+    cout << "prime";
+  else
+    cout << "not prime";
+
+  return 0;
+}
